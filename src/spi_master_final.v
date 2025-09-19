b1e426d50d733b153e74d34959bd8fe02e129de7
Alphin Francis <121490017+alphinfrancis25@users.noreply.github.com>
2025-09-19 23:19:41 +0530

    Update spi_master_final.v

--- src/spi_master_final.v
+++ src/spi_master_final.v
@@ -1,62 +1,64 @@
-module spi_master_final(clk,reset,sclk,din,miso,mosi,cs,start,dout);
+module spi_master_final(clk,reset,sclk,DIN,MISO,MOSI,cs,start,DOUT);
   input clk, reset ; 
-  input miso, start ;
-  input [7:0]din ;
+  input MISO, start ;
+  input [7:0] DIN ;
   output reg sclk ; 
-  output reg mosi ;
+  output reg MOSI = 0 ;
   output reg cs ;
-  output reg [7:0] dout ;
+  output reg [7:0] DOUT = 0 ;
   
-  reg [1:0] state ;
-  reg [7:0] shift_reg ;
-  reg [7:0] rx_reg ;
-  reg [3:0] count_bit ;
+  reg [1:0] STATE ;
+  reg [7:0] TX_reg ;
+  reg [7:0] RX_reg ;
+  reg [3:0] COUNT_BIT ;
   reg [3:0] clk_div ;
   
   localparam IDLE = 2'b00 , TRANSFER = 2'b01 , STOP = 2'b10;
   
   
-  always @(posedge clk or posedge reset)
-    begin
-      if(reset)
-        begin
-          sclk <= 1'b0 ;
-          clk_div <= 4'b0000 ;
-           
-        end
-      else
-         begin
-           if (clk_div == 4'b0100)begin
-             sclk <= ~sclk ;
-             clk_div <= 4'b0000 ;
-           end
-           else
-             clk_div <= clk_div + 4'b0001 ;
-         end
+    always @(posedge clk or posedge reset) begin
+      if (reset) begin
+        sclk <= 1'b0;
+        clk_div <= 4'b0000;
+      end else if (STATE != TRANSFER) begin  // Pause in IDLE/STOP (your change, good)
+        sclk <= 1'b0;
+        clk_div <= 4'b0000;
+      end else begin  // TRANSFER: Normal divide, but force first toggle
+        if (COUNT_BIT == 0 && clk_div == 0) begin  // First cycle after entry (COUNT_BIT=0 initial)
+          sclk <= ~sclk;  // Force immediate toggle (to neg if low)
+          clk_div <= 4'b0001;  // Start count from 1 for next
+        end else if (clk_div == 4'b0100) begin
+          sclk <= ~sclk;
+          clk_div <= 4'b0000;
+        end else
+          clk_div <= clk_div + 4'b0001;
+      end
     end
   
   always @(posedge clk or posedge reset)
     begin
       if(reset)
         begin
-          state <= IDLE ;
+          STATE <= IDLE ;
           cs <= 1'b1 ;
-          count_bit <= 4'b0000 ;
-          
+          COUNT_BIT <= 4'b0000 ;
+          RX_reg <= 8'b0 ;
+          TX_reg <= 8'b0 ;
         end
       else
         begin
-          case(state)
+          case(STATE)
             IDLE :
               
               begin
                 if(start)
                   begin
                     cs <= 1'b0 ;
-                    shift_reg <= din ;
-                    count_bit <= 4'b0000 ;
-                    rx_reg <= 8'b0 ;
-                    state <= TRANSFER ;
+                    TX_reg <= DIN ;
+                    COUNT_BIT <= 4'b0000 ;
+                    RX_reg <= 8'b0 ;
+                    STATE <= TRANSFER ;
+                    MOSI <= DIN[7] ;
                   end
                 else
                   begin
@@ -68,10 +70,9 @@ module spi_master_final(clk,reset,sclk,din,miso,mosi,cs,start,dout);
             STOP :
               
               begin
-                state <= IDLE ; 
-                dout <= rx_reg ;
-                count_bit <= 4'b0000 ;
-                cs <= 1'b1 ;
+                STATE <= IDLE ; 
+                DOUT <= RX_reg ;
+                COUNT_BIT <= 4'b0000 ;                
               end
             
           endcase
@@ -83,21 +84,23 @@ module spi_master_final(clk,reset,sclk,din,miso,mosi,cs,start,dout);
   
   always @(negedge sclk)
     begin
-      if(state == TRANSFER)
+      if(STATE == TRANSFER)
         begin
-          mosi <= shift_reg[7] ;
-          shift_reg <= { shift_reg[6:0],1'b0 } ;
+          MOSI <= TX_reg[7] ;
+          TX_reg <= { TX_reg[6:0],1'b0 } ;
         end
     end
   
   always @(posedge sclk)
     begin
-      if(state == TRANSFER)
+      if(STATE == TRANSFER)
         begin
-          rx_reg <= {rx_reg[6:0],miso} ;
-          count_bit <= count_bit + 1 ;
-          if(count_bit == 4'd8)
-            state <= STOP ;
+          RX_reg <= {RX_reg[6:0], MISO};
+          COUNT_BIT <= COUNT_BIT + 1 ;
+          if(COUNT_BIT == 4'd8)
+           begin
+            STATE <= STOP ;
+           end 
         end
     end
   
